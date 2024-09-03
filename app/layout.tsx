5bf27addc2739fb56351be31f3470642a2d3ca9b
lucas bittencourt rauch <lucas.bittencourtrauch@gmail.com>
2024-09-03 12:28:07 -0300

    implmentado clarity ms

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,15 @@ import "./globals.css";
 import { createTheme, ThemeProvider } from "@mui/material";
 import LayoutTabBar from "./components/LayoutTabBar";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
   },
-  typography : {
-    fontFamily : "Tahoma, sans-serif"
-  }
+  typography: {
+    fontFamily: "Tahoma, sans-serif",
+  },
 });
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,6 +24,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="microsoft-clarity-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                (function (c, l, a, r, i, t, y) {
+                c[a] =
+                c[a] ||
+                function () {
+                  (c[a].q = c[a].q || []).push(arguments);
+                };
+                t = l.createElement(r);
+                t.async = 1;
+                t.src = "https://www.clarity.ms/tag/" + i;
+                y = l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+              })(window, document, "clarity", "script", "nxh8ptjo77");`,
+        }}
+      />
       <body className={inter.className}>
         <ThemeProvider theme={darkTheme}>
           <LayoutTabBar />
